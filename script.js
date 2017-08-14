faae5b57f224bbe65e7265f6ea35161fc0c0b189
braau77 <30877997+braau77@users.noreply.github.com>
2017-08-14 10:43:46 -1000

    Added a file
    Uploaded a javascript and html file

---
+++ script.js
@@ -0,0 +1,9 @@
+function sayHello() {
+  //I am creating a function called sayHello//
+  var userName = prompt("What is your first name?");
+  //Created a variable called userName and stores the users information//
+  var nickName = prompt("What is your nickname")
+  var lastName = prompt("What is your last name?")
+  window.alert("Hello" + " " + userName + " " + nickName + " " +lastName);
+  //Popup a window that says hello to the user//
+}
