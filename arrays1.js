5295ccc128dcdaafb5be95b9bc0b142d6ba390c1
braau77 <30877997+braau77@users.noreply.github.com>
2017-08-16 10:23:27 -1000

    uploaded file
    uploaded practice arrays

---
+++ arrays1.js
@@ -0,0 +1,6 @@
+var juniors = ["Reimon", "Hana"];
+var seniors = ["Samiyah", "Justin", "Bryson"];
+window.alert("There are " + seniors.length + " seniors and " + juniors.length + " juniors")
+//show total number of juniors
+//show total number of seniors
+//make them all in a single popup window
