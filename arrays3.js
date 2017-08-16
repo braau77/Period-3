e4c39443386a328849a068e9e94090b76dbc4de4
braau77 <30877997+braau77@users.noreply.github.com>
2017-08-16 10:56:49 -1000

    Uploaded file
    added another array files

---
+++ arrays3.js
@@ -0,0 +1,7 @@
+//Ask user for a number
+//Ask user for another number
+//Multiply both numbers together
+//Output the result to the screen
+var valueOne = prompt("First number?");
+var valueTwo = prompt("Second number?");
+window.alert(valueOne*valueTwo);
