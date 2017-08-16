f667a0ffdbb7407e0774d0bf6cbf5c5fc9254b7d
braau77 <30877997+braau77@users.noreply.github.com>
2017-08-16 10:31:39 -1000

    uploaded file
    added an file about arrays

---
+++ arrays2.js
@@ -0,0 +1,10 @@
+var food = ["Sushi", " Salad", "Ramen"];
+//Ask the user for a food to replace Ramen
+	//create variable
+  //create prompt
+  //save user information as the created variable
+  //replace ramen in the array with the variable
+//Then display the food array values to the screen
+var replacement = prompt("Which you have? no ramen D:<");
+food[2] = replacement;
+window.alert(food);
