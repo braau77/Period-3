c126578419f1a53b50db3717a4321d5c949ecf48
braau77 <30877997+braau77@users.noreply.github.com>
2017-08-30 10:14:20 -1000

    Added file

---
+++ personName.js
@@ -0,0 +1,23 @@
+var personName = {
+	name: "personName",
+  age: 25,
+  grade: 6,
+  friends: ["dyson", "Matthew", "justin"]
+};
+var personName2 = {
+	name: "personName2",
+  age: 42,
+  grade: 3,
+  friends: ["Byson", "Matthew", "justin"]
+};
+var personName3 = {
+	name: "personName3",
+  age: 34,
+  grade: 2,
+  friends: ["Brydaddy", "Matthew", "Justin"]
+};
+var x = [personName, personName2, personName3];
+window.alert(x[2].friends[1]);
+
+
+
